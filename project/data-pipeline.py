e39adab010054819b67d0123a439b5ad5f686fcb
Fiona <fiona.sternberg@fau.de>
2024-11-09 13:21:43 +0100

    add more data cleaning

--- project/data-pipeline.py
+++ project/data-pipeline.py
@@ -23,11 +23,24 @@ def clean_data(dataset: str):
 
     data_sheet = df.parse('Data', header=3)
     data_sheet = data_sheet[data_sheet['Country Code'].isin(lac_country_codes)]
-    return data_sheet.drop(['Indicator Name', 'Indicator Code'], axis=1)
+
+    data_sheet.drop(['Indicator Name', 'Indicator Code'], axis=1, inplace=True)
+    columns_to_drop = [col for col in data_sheet.columns if col.isdigit() and (1960 <= int(col) <= 1999 or int(col) >= 2022)]
+    data_sheet.drop(columns=columns_to_drop, axis=1, inplace=True)
+
+    columns_to_check = [str(year) for year in range(2000, 2022)]
+    data_sheet = data_sheet.dropna(subset=columns_to_check, how='all')
+    return data_sheet.fillna(0)
+
 
 
 def write_to_database(dataset1, dataset2):
     print('Writing to database...')
+    remaining_countries = list(pd.merge(dataset1, dataset2, on='Country Code', how='inner')['Country Code'])
+
+    dataset1 = dataset1[dataset1['Country Code'].isin(remaining_countries)]
+    dataset2 = dataset2[dataset2['Country Code'].isin(remaining_countries)]
+
     conn = sqlite3.connect("../data/database.sqlite")
     dataset1.to_sql("life_expectancy", conn, if_exists="replace", index=False)
     dataset2.to_sql("health_expenditure", conn, if_exists="replace", index=False)
